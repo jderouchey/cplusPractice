235a39e4ac2a29666f4ecaa5d11c3bfcb9d2e6c9
joel.derouchey@gmail.com <joel.derouchey@gmail.com>
2017-09-13 08:34:39 -0700

    practice with cplus

---
+++ 3.cpp
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <iostream>
+
+
+
+// Notes:117 start 173 end
+
+using namespace std;
+
+int main(int lenArgs, char* args[])
+{
+	int page = 0;
+	if (lenArgs > 1){
+		page = atoi(args[1]);
+	}
+	else{
+		cout << "Please giv an argument to the program" << endl;
+		return 1;
+	}
+	
+	cout << ((page - 117)/173.0) * 100 
+	     << "Percentage completed in Chapter 3" 
+	     << endl;  
+
+	return 0;
+}
